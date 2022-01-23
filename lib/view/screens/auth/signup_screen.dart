43a915c6cd5e3e1923ff73c3bd001de1b4a7567e
bodour <git@github.com>
2022-01-23 20:19:04 +0300

    resolve the problem in Firebase in SignUp

--- lib/view/screens/auth/signup_screen.dart
+++ lib/view/screens/auth/signup_screen.dart
@@ -153,12 +153,22 @@ class SignupScreen extends StatelessWidget {
                           AuthButton(
                             text: 'Sign Up',
                             onPressed: () async {
-                              UserCredential userCredential = await FirebaseAuth
-                                  .instance
-                                  .createUserWithEmailAndPassword(
-                                      email: "barry.allen@example.com",
-                                      password: "SuperSecretPassword!");
-                              print(userCredential);
+                              try {
+                                UserCredential userCredential =
+                                    await FirebaseAuth.instance
+                                        .createUserWithEmailAndPassword(
+                                            email: "bodour@example.com",
+                                            password: "b123456789");
+                              } on FirebaseAuthException catch (e) {
+                                if (e.code == 'weak-password') {
+                                  print('The password provided is too weak.');
+                                } else if (e.code == 'email-already-in-use') {
+                                  print(
+                                      'The account already exists for that email.');
+                                }
+                              } catch (e) {
+                                print(e);
+                              }
                             },
                           ),
                         ],
